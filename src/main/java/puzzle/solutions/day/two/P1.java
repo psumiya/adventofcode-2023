84af82c6410a3e7f5c6b8c72d51a010a58c3b7f3
psumiya <sumiya.p@gmail.com>
2023-12-04 21:23:32 -0800

    Day 2 Problem 1

--- src/main/java/puzzle/solutions/day/two/P1.java
+++ src/main/java/puzzle/solutions/day/two/P1.java
@@ -4,15 +4,95 @@ import static puzzle.solutions.utils.Utils.isNullOrBlank;
 
 public class P1 {
 
-    public static int solve(String input) {
-        if (isNullOrBlank(input)) {
+    public static int solve(String games, int red, int green, int blue) {
+        if (isNullOrBlank(games)) {
             return 0;
         }
-        return process(input);
+        return process(games, red, green, blue);
     }
 
-    private static int process(String input) {
-        return 0;
+    private static int process(String games, int red, int green, int blue) {
+        int maxRed = 0, maxBlue = 0, maxGreen = 0;
+        String[] gamesArr = games.split("\n");
+        for (int i = 0; i < gamesArr.length; i++) {
+            String game = gamesArr[i];
+            String trimPrefix = game.substring(game.indexOf(":") + 2);
+            String[] cubeSet = trimPrefix.split(";");
+            for (String cubesGroup : cubeSet) {
+                String[] cubes = cubesGroup.split(",");
+                for (String cube : cubes) {
+                    if (cube.contains("red")) {
+                        int count = Integer.valueOf(cube.replaceAll("red", "").trim());
+                        if (count > maxRed) {
+                            maxRed = count;
+                        }
+                    } else if (cube.contains("green")) {
+                        int count = Integer.valueOf(cube.replaceAll("green", "").trim());
+                        if (count > maxGreen) {
+                            maxGreen = count;
+                        }
+                    } else if (cube.contains("blue")) {
+                        int count = Integer.valueOf(cube.replaceAll("blue", "").trim());
+                        if (count > maxBlue) {
+                            maxBlue = count;
+                        }
+                    } else {
+                        throw new RuntimeException("Unexpected cube color found, game=" + cube);
+                    }
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < gamesArr.length; i++) {
+            String game = gamesArr[i];
+            String trimPrefix = game.substring(game.indexOf(":") + 2);
+            String[] cubeSet = trimPrefix.split(";");
+            boolean isGamePossible = true;
+            for (String cubesGroup : cubeSet) {
+                String[] cubes = cubesGroup.split(",");
+                for (String cube : cubes) {
+                    if (cube.contains("red")) {
+                        int count = Integer.valueOf(cube.replaceAll("red", "").trim());
+                        if (count > red) {
+                            isGamePossible = false;
+                            break;
+                        }
+                    } else if (cube.contains("green")) {
+                        int count = Integer.valueOf(cube.replaceAll("green", "").trim());
+                        if (count > green) {
+                            isGamePossible = false;
+                            break;
+                        }
+                    } else if (cube.contains("blue")) {
+                        int count = Integer.valueOf(cube.replaceAll("blue", "").trim());
+                        if (count > blue) {
+                            isGamePossible = false;
+                            break;
+                        }
+                    } else {
+                        throw new RuntimeException("Unexpected cube color found, game=" + cube);
+                    }
+                }
+                if (!isGamePossible) {
+                    break;
+                }
+            }
+            if (isGamePossible) {
+                String indexStr = game.substring(5, game.indexOf(":"));
+                int index = Integer.parseInt(indexStr);
+                result += index;
+            }
+        }
+        return result;
+    }
+
+    private static String getArr(String[] items) {
+        StringBuilder sb = new StringBuilder();
+        for (String item : items) {
+            sb.append(item + " ");
+        }
+        return sb.toString();
     }
 
 }
